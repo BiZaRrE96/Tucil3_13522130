b56c0fdac11f703479cfe67fdf6a3eaec2b21667
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2024-05-07 07:38:24 +0700

    delete unused library

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.io.BufferedReader;
 
 public class GameTest {
